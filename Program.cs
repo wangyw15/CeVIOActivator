897d29fe25771aaadb4e5990443b3ff8500d659a
wangyw15 <wangyanwei15@outlook.com>
2023-12-11 21:40:22 +0800

    Automatically detect the installation path

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using Microsoft.Win32;
 
 namespace CeVIO_crack
 {
@@ -7,8 +9,12 @@ namespace CeVIO_crack
         // N54KC-7U2ZL-PQZBM-SPF8H suzuki trial key
         static void Main(string[] args)
         {
-            Console.Write("CeVIO*.exe: ");
-            var path = Console.ReadLine().Replace("\"","");
+            var path = GetCeVIOExecutable();
+            if (string.IsNullOrEmpty(path))
+            {
+                Console.Write("CeVIO AI.exe not found, please specify the file: ");
+                path = (Console.ReadLine() ?? "").Replace("\"", "");
+            }
             var activator = new Activator(path);
 
             Console.WriteLine("Loading...");
@@ -22,6 +28,34 @@ namespace CeVIO_crack
             Console.WriteLine("Completed");
             Console.WriteLine("You should reactivate CeVIO AI before " + DateTime.Now.AddDays(365).ToLongDateString());
             Console.ReadLine();
+        }
+
+        private static string GetCeVIOExecutable()
+        {
+            var folder = "";
+            using (var reg = Registry.LocalMachine.OpenSubKey("SOFTWARE\\CeVIO_NV\\Subject\\Editor\\x64"))
+            {
+                if (reg == null)
+                {
+                    return null;
+                }
+                folder = reg.GetValue("InstallFolder") as string;
+            }
+
+            if (string.IsNullOrEmpty(folder))
+            {
+                return null;
+            }
+
+            foreach (var file in Directory.GetFiles(folder))
+            {
+                if (Path.GetExtension(file).ToLower() == ".exe" && Path.GetFileName(file).StartsWith("CeVIO"))
+                {
+                    return file;
+                }
+            }
+
+            return null;
         }
     }
 }
